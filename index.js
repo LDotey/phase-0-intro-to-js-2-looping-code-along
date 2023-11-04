716b9d082908812d16fe5519dbbf22f6e436012a
Leigh Dotey <leighdotey@gmail.com>
2023-11-04 12:58:29 -0400

    while loop and looping

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+//const cards = (["Jessica", "Aaron", "Cynthia"], "pizza party")
+    function writeCards(names, event){
+        const thankYous = []
+         for (let i = 0; i < names.length; i++){
+            thankYous.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+         }
+         return thankYous;
+}
+
+function countDown(){
+    var i = 10
+    while (i >= 0){
+        console.log(i--);
+    }
+}
